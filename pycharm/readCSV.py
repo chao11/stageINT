551fa631b1881fd5c54c5ca7a2552607c924c0c0
hao.c <c.hao.11@groupe-esigelec.fr>
2016-03-11 18:10:20 +0100

    list save as dataframe

--- pycharm/readCSV.py
+++ pycharm/readCSV.py
@@ -1,6 +1,5 @@
 import pandas as pd
-import csv
-import numpy as np
+
 
 # read the csv file
 doc = pd.read_csv('VoiceLoc_Database_March16_Virginia.csv')
@@ -11,52 +10,29 @@ fileds = doc.columns
 DTI_scan_code = doc["DTI_SCAN_CODE"]
 
 Nb_sujet = 0
-DTIuse_code = []
+#DTIuse_code = []
+
+DTIuse_code = pd.DataFrame(columns=('Subject CODE', 'DTI_SCAN_CODE'))
+
 for row in range(0, len(DTI_scan_code)):
     # print row
-    str = DTI_scan_code[row]
+    code = DTI_scan_code[row]
     # print str
 
-    if str == str:  # check if it is not Nan,do:
-        if "_L" in str:
+    if code == code:  # check if it is not Nan,do:
+        if "_L" in code:
             # print('get one subject %d' % row)
             Nb_sujet += 1
+           # DTIuse_code.append(doc.loc[row, ['Subject CODE', 'DTI_SCAN_CODE']])
             DTIuse_code.append(doc.loc[row, ['Subject CODE', 'DTI_SCAN_CODE']])
             # sujetValid.writerow()
 
-# DTIuse_code.to_csv = ('VoiceLoc_Database_March16_Virginia.csv')
 
 
-print('nombre de sujet DTIcode valide: %d' % Nb_sujet)
+print('nombre de sujet DTIcode valide: %d ' % Nb_sujet)
 
 print DTIuse_code
-"""
 
+DTIuse_code.to_excel('/hpc/crise/hao.c/DTIsujet.xlsx')
 
-print(DTI_scan)
-
-
-with open('VoiceLoc_Database_March16_Virginia.csv','rb')as csvfile:
-    write = csv.writer(csvfile) # write into csv
-
-    firstrow = csvfile.readlines(1)
-    filednames = tuple(firstrow[0].strip('\n').split('\t')) # get the fieldname
-    print (filednames)
-
-    for row in DTI_scan:
-        if DTI_scan[row] == 'Y':
-            write.writerow = csvfile(row)
-
-    print(write)
-
-    print (DTI_scan)
-
-# DTI_scan = pd.DataFrame(doc, columns=[['DTI SCAN [Yes/No]']=='Y'])
-"""
-"""
-for row in range(0,len(DTI_scan)):
-    if DTI_scan(row) == 'Y':
-        DTIidx = row
-
-print(DTIidx)
-"""
+DTIuse_code.to_excel = ('VoiceLoc_Database_March16_Virginia.xlsx', 'sheet1')
