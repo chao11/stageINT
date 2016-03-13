d0f974a3ea7e480bd693e927989380bf01b9817b
chang <lily.1993@live.cn>
2016-03-13 13:06:48 +0100

    Added files via upload
    select and save as csv

--- pycharm/readCSV.py
+++ pycharm/readCSV.py
@@ -3,14 +3,18 @@ import pandas as pd
 
 # read the csv file
 doc = pd.read_csv('VoiceLoc_Database_March16_Virginia.csv')
-# name of the fields
+DTI = doc[doc['DTI SCAN [Yes/No]']=='Y']
+print ("subjects have DTI scan : %d" % len(DTI))
+
+
 fileds = doc.columns
 
 # extract the colonne of the DTIscan code and chack if the code is valided with '_L'
 DTI_scan_code = doc["DTI_SCAN_CODE"]
 
 Nb_sujet = 0
-#DTIuse_code = []
+index = 0
+
 
 DTIuse_code = pd.DataFrame(columns=('Subject CODE', 'DTI_SCAN_CODE'))
 
@@ -23,16 +27,16 @@ for row in range(0, len(DTI_scan_code)):
         if "_L" in code:
             # print('get one subject %d' % row)
             Nb_sujet += 1
-           # DTIuse_code.append(doc.loc[row, ['Subject CODE', 'DTI_SCAN_CODE']])
-            DTIuse_code.append(doc.loc[row, ['Subject CODE', 'DTI_SCAN_CODE']])
-            # sujetValid.writerow()
-
+            DTIuse_code.loc[index] = doc.loc[row, ['Subject CODE', 'DTI_SCAN_CODE']]
+            index += 1
 
 
 print('nombre de sujet DTIcode valide: %d ' % Nb_sujet)
 
-print DTIuse_code
+print (DTIuse_code)
 
-DTIuse_code.to_excel('/hpc/crise/hao.c/DTIsujet.xlsx')
+# DTIuse_code.to_excel('/hpc/crise/hao.c/DTIsujet.xlsx')
+DTIuse_code.to_csv('subject with valide DTIcode.csv')
+DTIuse_code.to_excel = ('D:\stage\stageINT-master\stageINT-master\pycharm\valide_sujet_List.xlsx', 'sheet1')
 
-DTIuse_code.to_excel = ('VoiceLoc_Database_March16_Virginia.xlsx', 'sheet1')
+pd.read_csv('subject with valide DTIcode.csv')
