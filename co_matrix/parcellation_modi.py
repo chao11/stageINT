721bd890f2126ea118f00dfb062e088b02b23da3
hao.c <c.hao.11@groupe-esigelec.fr>
2016-04-13 15:16:45 +0200

    seed parcellation using ward's method: X=load() connect = joblib.load('*.jl'), conn_matrix = connect[0] test: ward.fit(conect_matrix)

--- co_matrix/parcellation_modi.py
+++ co_matrix/parcellation_modi.py
@@ -3,6 +3,7 @@
 
 import sys
 import nibabel as nib
+import joblib
 import commands
 
 import os
@@ -18,68 +19,69 @@ from scipy import sparse
 # nb_cluster = int(sys.argv[4])
 nb_cluster = 2
 
-#nom de la tracto : CC_RH, M1_LH, Thalamus_RH, etc
-# name_tracto = sys.argv[6]
-name_tracto = 'tarcto'
-
-# nom du conteneur : Tracto dans mon cas
-tracto_basename = sys.argv[5]
-
-# seed/target name : FROM ROOT SEGMENTATION PATH (so add Right_H/Left_H if needed)
-seed_name = 'seed.nii.gz'
-
-#path of the subject (patient/controls)
-subject_path = sys.argv[7]
-
-
 root_dir = '/hpc/crise/hao.c/data'
 subjects_list = os.listdir(root_dir)
 
-for subject in subjects_list:
-#for ind, arg in enumerate(sys.argv[1:]):
+for subject in subjects_list[0:1]:
 #    subject = arg
-
-    # define directories
+     # define directories
     subject_dir = op.join(root_dir,subject)
     fs_seg_dir = op.join(subject_dir,'freesurfer_seg')
     seed_path = op.join(fs_seg_dir,'lh_STS+STG.nii.gz')
     target_path = op.join(fs_seg_dir,'target_mask.nii.gz')
-    output_tracto_dir = op.join(subject_dir,'tracto','RH_STS+STG_destrieux')
+    output_tracto_dir = op.join(subject_dir,'tracto','LH_STS+STG_destrieux')
     fdt_fullmatrix_path = op.join(output_tracto_dir,'fdt_matrix2.dot')
-    output_path = op.join(output_tracto_dir,'conn_matrix_seed2parcels.jl')
+    conn_matrix_path = op.join(output_tracto_dir,'conn_matrix_seed2parcels.jl')
+
+    output_name = 'parcellisation_ward_2cluster.nii.gz'
+    output_path = op.join(output_tracto_dir,output_name)
 
     #==============================================================================
     #loading the datas : seed masks, target masks and connectivity matrix fdt_matrix2.dot
     #loading the seed
     seedH = nib.load(seed_path)
-    #
     seed = seedH.get_data()
+    mask = seed.astype(np.bool)
+    shape = mask.shape
 
-
-
+    # m1 : how many non-zeros in seed (could have used numpy nonzero function...)
+    m1 = sum(sum(sum(1 for i in row if i > 0) for row in row2) for row2 in seed)
+    # index of a voxel given its position X,Y,Z
+    connect_use = np.zeros((shape[0],shape[1],shape[2]))
+    # position of avoxel X,Y,Z given its index
+    connect_use2 = np.zeros((m1,3))
+    #
+    index=0
+    #
+    # ne pas utiliser NP.WHERE OU AUTRE nonzero
+    for k in range(shape[2]):
+    #
+        for j in range(shape[1]):
+    #
+            for i in range(shape[0]-1,-1,-1):
+    #
+                if seed[i, j, k] > 0:
+    #
+                    connect_use[i,j,k]=index
+    #
+                    connect_use2[index]=[i,j,k]
+    #
+                    index=index+1
 
     #==============================================================================
     # CLUSTERING STAGE
     # USE WARD
-    # sometime there are nan in the matrix after cross-correlation due to full zero line in the
-    # former matrix. Hence, one replace the non-zeros with almost zero
-    connect = np.nan_to_num(connect)
+    connect = joblib.load(conn_matrix_path)
 
-    #   loading the mask, and binarise
-
-    mask = seed.astype(np.bool)
-    #
-    shape = mask.shape
-    #
     print 'compute adjacency matrix...'
     # compute the adjacency matrix over the target mask
     from sklearn.neighbors import kneighbors_graph
-    connectivity = kneighbors_graph(connect_use2,7)
+    connectivity = kneighbors_graph(connect_use2, 7)
 
     print 'ward clustering...'
     #   perform a hierarchical clustering considering spatial neighborhood
     ward = WardAgglomeration(n_clusters = nb_cluster, connectivity=connectivity)
-    ward.fit(np.transpose(connect))
+    ward.fit(connect_use2)
     labelsf = ward.labels_
 # the labels are the final labels of each voxels
 
@@ -104,5 +106,3 @@ for subject in subjects_list:
     sFinal=nib.Nifti1Image(parcellation_data, seedH.get_affine())
     nib.save(sFinal, output_path)
 
-    if option_percent!=1:
-        connectivity_matrix_file.close()
