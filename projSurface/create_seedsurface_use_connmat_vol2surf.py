d0597579e324123b2ea8ec58857eca8a2b4c1308
hao.c <c.hao.11@groupe-esigelec.fr>
2016-06-13 10:39:20 +0200

    compute the surfacic connectivity matrix

--- projSurface/create_seedsurface_use_connmat_vol2surf.py
+++ projSurface/create_seedsurface_use_connmat_vol2surf.py
@@ -31,6 +31,8 @@ connmat_path = op.join(tracto_path,'conn_matrix_seed2parcels.jl')
 coord_file_path = '/hpc/crise/hao.c/data/AHS22/tracto/LH_STS+STG_destrieux/coords_for_fdt_matrix2'
 seed_nii = nib.load('/hpc/crise/hao.c/data/AHS22/freesurfer_seg/lh_STS+STG.nii.gz')
 surface_dir = op.join(root_dir, subject, 'surface')
+seed_gii = '/hpc/crise/hao.c/data/AHS22/freesurfer_seg/lh_seed.gii'
+
 
 with open(coord_file_path,'r') as f:
     file = f.read()
@@ -53,6 +55,9 @@ mat = jl.load(connmat_path)[0]
 nii = np.zeros((256,256,256))
 n_target = mat.shape[1]
 
+size = len(ng.read(seed_gii).darrays[0].data)
+g_data = np.zeros((size, n_target))
+
 # compute the profile (nifti image) for each target
 for t in range(n_target):
     valeur = mat[:,t]
@@ -63,44 +68,39 @@ for t in range(n_target):
         nii[c[0],c[1],c[2]] = valeur[j]
 #   save the nifti image
     img = nib.Nifti1Image(nii, seed_nii.get_affine())
-    nii_output_path = op.join(surface_dir, '{}.nii'.format(t))
+    nii_output_path = op.join(surface_dir, '%03d.nii'%(t+1))
     img.to_filename(nii_output_path)
 
 #   peoject the profil onto surface
     gii_text_output_path = op.join(surface_dir, '{}.gii'.format(t))
     proj_cmd = '%s/mri_vol2surf --src %s --o %s --out_type gii --regheader %s --hemi %s %s  ' % (fs_exec_dir, nii_output_path, gii_text_output_path, subject, hemi, projection_method )
-    #commands.getoutput(proj_cmd)
-    print proj_cmd
+    commands.getoutput(proj_cmd)
+    #print proj_cmd
 
-
-# relaod the gifti files and cumpute the seed surface mask
-size = len(ng.read(gii_text_output_path).darrays[0].data)
-connmat_proj = np.zeros((size, n_target))
-
-for t in range(n_target):
-    gii_text_output_path = op.join(surface_dir, '{}.gii'.format(t))
+#   reload gifti file
     g = ng.read(gii_text_output_path).darrays
-    connmat_proj[:,t] = g[0].data
-# save the projected connmat
-connmat_prjo_path = op.join(tracto_path, 'connmat_proj.jl')
-jl.dump(connmat_proj, connmat_prjo_path, compress=3)
-print "connmat_proj saved "
+    g_data[:,t] = g[0].data
 
-# sum of theprofile for each target
-sum = connmat_proj.sum(axis=1)
+#   remove gifti and nifti file
+    os.remove(nii_output_path)
+    os.remove(gii_text_output_path)
+
+print 'shape of gift data: {} '.format(g_data.shape)
+
+# extract the profile of the seed region and compute the profile project on the surface
+sum = g_data.sum(axis=1)
 seedroi_gii = np.zeros((size,))
-idx = np.where(sum!=0)
-seedroi_gii[np.where(sum!=0)]=1
+idx = np.where(sum!=0)[0]
+seedroi_gii[idx[0]]=1
 
-seed_gii_path = op.join(surface_dir, '{}_seed.gii'.format(hemi))
-ng.write(seedroi_gii, seed_gii_path)
+connmat_proj = g_data[idx,:]
+
+print("shape of the surfacic connmat :{}".format(connmat_proj.shape))
+
+# save the projected connmat
+connmat_proj_path = op.join(tracto_path, 'surfacic_connectivity_profile_STSSTG_{}.jl'.format(hemi.lower()))
+jl.dump(connmat_proj, connmat_proj_path, compress=3)
+print "surfacic_connectivity_profile saved "
 
 
 
-
-# compare with the projection of seed_msk and fdy_path
-seed_mask_vlo2surf = '/hpc/crise/hao.c/data/AHS22/freesurfer_seg/lh_seed.gii'
-g_seed = ng.read(seed_mask_vlo2surf).darrays
-
-fdtpath_surface = '/hpc/crise/hao.c/data/AHS22/surface/surf_proj_fdtpath.gii'
-fdt_gii = ng.read(fdtpath_surface).darrays
