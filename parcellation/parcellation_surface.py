79924080e1a0c897e66ea53d3b79864a7877b591
hao.c <c.hao.11@groupe-esigelec.fr>
2016-06-15 15:31:55 +0200

    correct the mesh: use *h.white as surface mesh file

--- parcellation/parcellation_surface.py
+++ parcellation/parcellation_surface.py
@@ -1,6 +1,10 @@
 # hao.c
 # 06/06/2016
-# Ward parcellation of connectivity matrix on surface
+"""
+Ward parcellation of connectivity matrix on surface (surfacic_connectivity_profile_STSSTG.jl)
+the parcellation result is saved in the directory "surface" of the correspondence subject
+
+"""
 
 from sklearn.cluster import WardAgglomeration
 from sklearn.cluster import AgglomerativeClustering
@@ -16,127 +20,70 @@ import sys
 import joblib as jl
 import commands
 
-
-hemi = 'rh'
+"""
+hemi = 'lh'
 altas = 'destrieux'
-nb_clusters = 4
+nb_clusters = 3
+subject = 'AMA02'
+
+"""
+subject = str(sys.argv[1])
+hemi = str(sys.argv[2])
+nb_clusters = int(sys.argv[3])
+altas = str(sys.argv[4])
+
+surface = 'white'
 
 # freesurfer setup:
 fs_exec_dir = '/hpc/soft/freesurfer/freesurfer/bin'
-# projection_method = '--projfrac-avg 0 1 0.1'
-projection_method = '--projfrac 0.5'
-
+projection_method = '--projfrac-avg 0 1 0.1'
+#projection_method = '--projfrac 0.5'
+fs_subject_dir ="/hpc/banco/voiceloc_full_database/fs_5.3_sanlm_db"
 
 root_dir = '/hpc/crise/hao.c/data'
-subject = 'AHS22'
+subject_list = os.listdir(root_dir)
+
 subject_path = op.join(root_dir,subject)
 tracto_name = '{}_STS+STG_{}'.format(hemi.upper(), altas)
 tracto_dir = op.join(subject_path, 'tracto', tracto_name)
-surface_dir = op.join(root_dir, subject, 'surface')
-parcellation_path=op.join(tracto_dir,'parcel_surface')
+surface_dir = op.join(subject_path, 'surface')
 
-mesh_path = op.join(subject_path,'surface', 'lh_surf_proj_inflated_fdtpath.gii')
+inflated_surface_path = op.join(fs_subject_dir, subject, 'surf', '{}.inflated'.format(hemi.lower()))
+white_surface_path = op.join(fs_subject_dir, subject, 'surf', '{}.white'.format(hemi))
+# mesh is the *h.white.gii
+mesh_path = op.join(surface_dir,'{}.white.gii'.format(hemi))
 # path_target = op.join(subject_path,'freesurfer_seg', 'target_mask_destrieux.nii.gz')
 
-seegroi_nii_path = op.join(subject_path,'freesurfer_seg', '{}_STS+STG.nii.gz'.format(hemi))
 seedroi_gii_path = op.join(subject_path,'freesurfer_seg', '{}_STS+STG.gii'.format(hemi))
-
-surfacic_connmat_path = op.join(tracto_dir, 'surfacic_connectivity_profile_STSSTG_{}.jl'.format(hemi.lower()))
-gii_tex_parcellation_path=op.join(parcellation_path,'parcellation_cl{}.gii'.format(nb_clusters))
 
 connmat_path = op.join(tracto_dir,'conn_matrix_seed2parcels.jl')
 connmat_proj_path = op.join(tracto_dir, 'surfacic_connectivity_profile_STSSTG_{}.jl'.format(hemi.lower()))
-coord_file_path = op.join(tracto_dir, 'coords_for_fdt_matrix2')
 
-seed_nii = nib.load(seegroi_nii_path)
+output_gii_parcellation_path=op.join(surface_dir,'norma_{}.{}.parcellation_cl{}.gii'.format(hemi, altas, nb_clusters))
 
 print 'subject ID: {}\n surface parcellation for {}, altas = {}, number of clusters = {}'.format(subject, hemi, altas, nb_clusters)
 
-# ============== project the seed roi volume mask onto surface==========================================================
-if not op.isfile(seedroi_gii_path):
-    print 'project the seed roi volume mask onto surface'
-    proj_cmd = '%s/mri_vol2surf --src %s --o %s --out_type gii --regheader %s --hemi %s %s  ' % (fs_exec_dir, seegroi_nii_path, seedroi_gii_path, subject, hemi, projection_method )
-    commands.getoutput(proj_cmd)
-    print 'seed roi gii: {}'.format(seedroi_gii_path)
+print surface_dir
+# convert the mesh gifti file
+if not op.isdir(surface_dir):
+    os.mkdir(surface_dir)
 
+if not op.isfile(mesh_path):
+    print'*h.white.gii not exists yet, convert now.......'
 
-# ================== compute surfacic connectivity matrix===============================================================
-print 'compute surfacic connectivity matrix......'
-
-with open(coord_file_path,'r') as f:
-    file = f.read()
-    text = file.split('\n')
-    # print("total number of voxels:", len(text)-1)
-    seed_coord = np.zeros((len(text)-1, 3), dtype= int)
-
-    for i in range(len(text)-1):
-    #
-        data = np.fromstring(text[i], dtype=int, sep=" ")
-    #
-        seed_coord[i, 0] = data[0]
-    #
-        seed_coord[i, 1] = data[1]
-    #
-        seed_coord[i, 2] = data[2]
-
-mat = jl.load(connmat_path)[0]
-nii = np.zeros((256,256,256))
-n_target = mat.shape[1]
-
-size = len(ng.read(seedroi_gii_path).darrays[0].data)
-g_data = np.zeros((size, n_target))
-
-# compute the profile (nifti image) for each target
-for t in range(n_target):
-    valeur = mat[:,t]
-
-#   compute the nifti image corresponding to this target
-    for j in range(len(seed_coord)):
-        c = seed_coord[j]
-        nii[c[0],c[1],c[2]] = valeur[j]
-#   save the nifti image
-    img = nib.Nifti1Image(nii, seed_nii.get_affine())
-    nii_output_path = op.join(surface_dir, '%03d.nii'%(t+1))
-    img.to_filename(nii_output_path)
-
-#   peoject the profil onto surface
-    gii_text_output_path = op.join(surface_dir, '{}.gii'.format(t))
-    proj_cmd = '%s/mri_vol2surf --src %s --o %s --out_type gii --regheader %s --hemi %s %s  ' % (fs_exec_dir, nii_output_path, gii_text_output_path, subject, hemi, projection_method )
-    commands.getoutput(proj_cmd)
-    #print proj_cmd
-
-#   reload gifti file
-    g = ng.read(gii_text_output_path).darrays
-    g_data[:,t] = g[0].data
-
-#   remove gifti and nifti file
-    os.remove(nii_output_path)
-    os.remove(gii_text_output_path)
-
-print 'shape of gift data: {} '.format(g_data.shape)
-
-# extract the profile of the seed region and compute the profile project on the surface
-sum = g_data.sum(axis=1)
-seedroi_gii = np.zeros((size,))
-idx = np.where(sum!=0)[0]
-seedroi_gii[idx[0]]=1
-
-connmat_proj = g_data[idx,:]
-
-print("shape of the surfacic connmat :{}".format(connmat_proj.shape))
-
-# save the projected connmat
-jl.dump(connmat_proj, connmat_proj_path, compress=3)
-print "surfacic_connectivity_profile saved \n {} \n".format(connmat_proj_path)
-
+    cmd = '%s/mris_convert %s  %s' %(fs_exec_dir, white_surface_path, mesh_path)
+    # print cmd
+    commands.getoutput(cmd)
 
 # ===================parcellation ======================================================================================
 
-
 # load connectivity matrix
-
-surf_connmat = jl.load(surfacic_connmat_path)
+surf_connmat = jl.load(connmat_proj_path)
 print "surfacic connectivity matrix: {} ".format(surf_connmat.shape)
+
+# normalize the connmat
+from sklearn.preprocessing import normalize
+surf_connmat = normalize(surf_connmat,norm='l2')
 
 # read surface seed roi mask
 seedroi_gii = ng.read(seedroi_gii_path)
@@ -158,6 +105,8 @@ ward = AgglomerativeClustering(n_clusters = nb_clusters, linkage='ward',connecti
 ward.fit(surf_connmat)
 labelsf = ward.labels_
 
+for i in range(len(np.unique(labelsf))):
+    print 'label %d: %d' % (i,len(labelsf[labelsf==i]))
 # write the final gifti parcellation
 print 'write parcellation.gii'
 ii=0
@@ -165,9 +114,11 @@ for i in surfmask_inds:
     seedroi_gii.darrays[0].data[i]=labelsf[ii]+2
     ii=ii+1
 
-# save the parcellation:
-if not op.isdir(parcellation_path):
-    os.mkdir(parcellation_path)
 
-ng.write(seedroi_gii,gii_tex_parcellation_path)
-print 'save parcellation:\n'+gii_tex_parcellation_path
+ng.write(seedroi_gii,output_gii_parcellation_path)
+print 'save parcellation:\n'+output_gii_parcellation_path
+
+# visualization of the parcellation in freeview, overlay on the inflated sueface
+cmd_freeview = '%s/freeview -f %s:overlay=%s &' %(fs_exec_dir, inflated_surface_path, output_gii_parcellation_path)
+print(cmd_freeview)
+# commands.getoutput(cmd_freeview)  # 'freeview.bin: Command not found.'
