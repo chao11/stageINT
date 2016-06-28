c09f83e5ad8c8d5db6fa2c3df047e5ac315e1ee6
hao.c <c.hao.11@groupe-esigelec.fr>
2016-06-28 11:31:59 +0200

    save ghe r2 score as dataframe ad save in existing excel file

--- modeling/LOSO_RL_loopsubject.py
+++ modeling/LOSO_RL_loopsubject.py
@@ -19,7 +19,6 @@ import matplotlib.pylab as plt
 import commands
 from sklearn.metrics import r2_score
 import pandas as pd
-from openpyxl import load_workbook
 
 
 def learn_model(x_train, y_train, x_test, y_test):
@@ -107,10 +106,22 @@ def get_data(x_path, y_path, seed_coord):
     return x,y
 
 
-def normaliser(x):
-  # Nomaliser:
-    from sklearn.preprocessing import normalize
-    x_norma = normalize(x,norm='l2')
+def normaliser(x, option):
+  # normalize by the norm
+    if option=='l2':
+       # print 'normalize by the norm or the row'
+        from sklearn.preprocessing import normalize
+        x_norma = normalize(x,norm='l2')
+
+#   normalize by the sum of the row, ( normalized matrix sum to 1 )
+    elif option=='l1': # normalize sum to 1:
+        #print('normalize by the sum of the row')
+        from sklearn.preprocessing import normalize
+        x_norma = normalize(x,norm='l1')
+    elif option=='none':
+       # print ('no normalization')
+        x_norma = x
+
     return x_norma
 
 
@@ -124,6 +135,15 @@ def remove_nan(x, y):
         x = np.delete(x, nan_ind, 0)
         y = np.delete(y, nan_ind, 0)
     return x, y, nan_ind
+
+
+ #   get the number of waytatal:
+def get_waytotal(file_path):
+    with open(file_path, 'r')as f:
+        file = f.read()
+        text = file.split('\n')
+        waytotal = np.fromstring(text[0], dtype=int, sep=" ")
+    return waytotal
 
 
 def loso_model(hemisphere, parcel_altas, model, y_file, norma):
@@ -189,7 +209,7 @@ def loso_model(hemisphere, parcel_altas, model, y_file, norma):
         subject_test= [subjects_list[index] for index in test_index]
         subject_train= [subjects_list[index] for index in train_index]
 
-    #   base de test:
+#      base de test:==========================================================================================
         print "sujet test: " ,subject_test[0]
         x_test_path = op.join(root_dir, subject_test[0], filename)
         y_test_path = op.join(fMRI_dir, subject_test[0],y_basedir )
@@ -199,7 +219,16 @@ def loso_model(hemisphere, parcel_altas, model, y_file, norma):
         print("load X_test: " + x_test_path + "\n load Y_test: " + y_test_path )
         X_test, Y_test = get_data(x_test_path, y_test_path, test_seed_coord)
 
-    #   base d'apprentissage:
+#       normalize
+    #   get the number of waytatal:
+        if norma =='waytotal':
+            waytotal_file = op.join(root_dir, subject_test[0], tracto_dir, 'waytotal')
+            nb_waytotal = get_waytotal(waytotal_file)
+            X_test = X_test/nb_waytotal
+        else:
+            X_test = normaliser(X_test, norma)
+
+#      base d'apprentissage:==================================================================================
         # print("train:", subject_train)
         X_train = np.empty((0,nb_target), float)
         Y_train = np.empty(0, float)
@@ -212,29 +241,30 @@ def loso_model(hemisphere, parcel_altas, model, y_file, norma):
             train_seed_coord = read_coord(train_coord_path)
 #
             X, Y = get_data(x_train_path,y_train_path, train_seed_coord)
+
+#           normalize
+            if norma =='waytotal':
+                waytotal_file = op.join(root_dir, subject, tracto_dir, 'waytotal')
+                X = X/get_waytotal(waytotal_file)
+            else:
+                X = normaliser(X, norma)
+
             X_train = np.append(X_train, X, axis=0)
             Y_train = np.append(Y_train, Y, axis=0)
 
         print X_train.shape, Y_train.shape
 
 
-    #   check for nan values:
-        X_test,Y_test, test_nan_id = remove_nan(X_test,Y_test)
+#       check for nan values:
+        X_test, Y_test, test_nan_id = remove_nan(X_test,Y_test)
         X_train, Y_train, nan_train  = remove_nan(X_train, Y_train)
 
         if len(test_nan_id)>0:
             print "remove the seed voxel:", test_nan_id
             test_seed_coord = np.delete(test_seed_coord, test_nan_id, 0)
 
-    #   normalization:
-        if norma == 1:
-            print "normaliser la matrice"
-            X_train = normaliser(X_train)
-            X_test = normaliser(X_test)
-        else:
-            print "non normaliser"
 
-    #   LEARN MODEL
+#       LEARN MODEL
         coeff, predi, MAE[test_index] = learn_model(X_train, Y_train, X_test, Y_test)
         r2[test_index] = r2_score(Y_test, predi)
         print 'r2: ', r2[test_index]
@@ -244,39 +274,34 @@ def loso_model(hemisphere, parcel_altas, model, y_file, norma):
         if not op.isdir(predict_subject):
             os.mkdir(predict_subject)
 
-        predict_output = op.join(predict_subject, '{}_{}_{}_predi_{}.jl'.format(hemisphere, model, parcel_altas,  y_file))
-
+        predict_output = op.join(predict_subject, '{}_{}_{}_{}_predi_{}.jl'.format(hemisphere, model, norma, parcel_altas,  y_file))
         joblib.dump([test_seed_coord, predi], predict_output, compress=3)
 
-        # save the predict map
+#       save the predict map
         predict_nii = np.zeros((256,256,256))
         for i in range(len(test_seed_coord)):
             c = test_seed_coord[i]
             predict_nii[c[0],c[1],c[2]] = predi[i]
 
         img = nib.Nifti1Image(predict_nii, nib.load(y_test_path).get_affine())
-        predict_output_path = op.join(predict_subject, '{}_{}_{}_predi_{}.nii.gz'.format(hemisphere, model, parcel_altas, y_file))
+        predict_output_path = op.join(predict_subject, '{}_{}_{}_{}_predi_{}.nii.gz'.format(hemisphere, model, norma, parcel_altas, y_file))
         img.to_filename(predict_output_path)
 
 
     print "mean MAE:", np.mean(MAE)
 
-  #  plt.plot(MAE)
-  #  plt.title('MAE of %s_%s_%s_%s, mean: %s' %(hemisphere, parcel_altas, model, y_file, str(np.mean(MAE))))
-  #  plt.legend()
-  #  plt.savefig('/hpc/crise/hao.c/model_result/{}_{}_{}_{}_norma{}.png'.format(hemisphere, parcel_altas, model, y_file, str(norma)))
-
     print "mean R2 score:", np.mean(r2)
 
-  #  plt.plot(r2)
-  #  plt.title('r2 of %s_%s_%s_%s, mean: %s' %(hemisphere, parcel_altas, model, y_file, str(np.mean(r2))))
-  #  plt.savefig('/hpc/crise/hao.c/model_result/r2score_{}_{}_{}_{}_norma{}.png'.format(hemisphere, parcel_altas, model, y_file, str(norma)))
+    plt.plot(r2)
+    plt.legend()
+    plt.title('r2 of %s_%s_%s_%s, mean: %s' %(hemisphere, parcel_altas, model, y_file, str(np.mean(r2))))
+    plt.savefig('/hpc/crise/hao.c/model_result/r2score_{}_{}_{}_{}_norma{}.png'.format(hemisphere, parcel_altas, model, y_file, str(norma)))
 
 #   write to DataFrame
-    d = {'MAE': pd.Series(MAE,index=subjects_list), 'R2_score':pd.Series(r2, index=subjects_list) }
-    df = pd.DataFrame(d)
+    # d = {'MAE': pd.Series(MAE,index=subjects_list), 'R2_score':pd.Series(r2, index=subjects_list) }
+    # df = pd.DataFrame(d)
 
-    return df
+    return MAE, r2, subjects_list
 
 
 # ==================== main============================================================
@@ -284,16 +309,15 @@ def loso_model(hemisphere, parcel_altas, model, y_file, norma):
 #hemisphere = ['lh','rh']
 #parcel_altas = ['aparcaseg', 'wmparc']
 #model = 'distance'
-y_file = ['rspmT_0001','rspmT_0002','rspmT_0003','rspmT_0004', 'rcon_0001', 'rcon_0002', 'rcon_0003', 'rcon_0004']
-#y_file = ['rspmT_0001','rcon_0001']
+#y_file = ['rspmT_0001','rspmT_0002','rspmT_0003','rspmT_0004', 'rcon_0001', 'rcon_0002', 'rcon_0003', 'rcon_0004']
+
+y_file = ['rspmT_0001','rcon_0001']
 
 hemisphere = str(sys.argv[1])
 parcel_altas = str(sys.argv[2])
 model = str(sys.argv[3])
 #y_file = str(sys.argv[4])
-
-
-norma = 1
+options = ['none', 'waytotal', 'l1', 'l2']
 
 fs_exec_dir = '/hpc/soft/freesurfer/freesurfer/bin'
 
@@ -302,39 +326,46 @@ subjects_list = os.listdir(root_dir)
 fMRI_dir = '/hpc/banco/voiceloc_full_database/func_voiceloc'
 tracto_dir = 'tracto/{}_STS+STG_{}_2/'.format(hemisphere.upper(),parcel_altas)
 
-output_predict_score_excel_file = '/hpc/crise/hao.c/model_result/LOSO_result.xlsx'
-
+dict_R2 = {}
 dlist = []
 for y in y_file:
 
-    y_basedir = 'nomask_singletrialbetas_spm12_stats/resampled_fs5.3_space/{}.nii'.format(str(y))
+    # output_predict_score_excel_file = '/hpc/crise/hao.c/model_result/LOSO_result_compare_normalization.xlsx'
+    output_predict_score_excel_file = '/hpc/crise/hao.c/model_result/LOSO_R2score_compare_normalization.xlsx'
 
-    if model == 'distance':
-        filename = op.join('control_model_distance','{0}_distance_control_{1}.jl'.format(hemisphere, parcel_altas))
+    for norma in options:
 
-    else:
-        filename = op.join(tracto_dir, 'conn_matrix_seed2parcels.jl')
+        y_basedir = 'nomask_singletrialbetas_spm12_stats/resampled_fs5.3_space/{}.nii'.format(str(y))
 
-    print "modeling %s altas: %s, \nY :%s, \nX:%s, norma:%s \n" %(hemisphere, parcel_altas, y, filename, str(norma))
+        if model == 'distance':
+            filename = op.join('control_model_distance','{0}_distance_control_{1}.jl'.format(hemisphere, parcel_altas))
 
-#   modeling:
-    result_dataframe = loso_model(hemisphere, parcel_altas, model, y, norma)
+        else:
+            filename = op.join(tracto_dir, 'conn_matrix_seed2parcels.jl')
 
+        print "modeling %s altas: %s, \nY :%s, \nX:%s, norma:%s \n" %(hemisphere, parcel_altas, y, filename, str(norma))
+
+    #   modeling:
+        mea, R2, sub_list= loso_model(hemisphere, parcel_altas, model, y, norma)
+        dict_R2 [norma]= pd.Series(R2, index=sub_list)
+
+    #   mean score of this set of LOSO
+        d = {'altas': parcel_altas, 'hemisphere': hemisphere, 'model':model, 'norma':norma,  'y_file' :y, 'mean_MAE': result_dataframe.mean(0)[0], 'R2':result_dataframe.mean(0)[1]}
+        dlist.append(d)
+
+
+    result_dataframe = pd.DataFrame(dict_R2)
 #   save the predict file to a new worksheet
-#   load excel file(workbook)
-    book = load_workbook(output_predict_score_excel_file)
-    writer = pd.ExcelWriter(output_predict_score_excel_file, engine='openpyxl')
-    writer.book = book
-    writer.sheets = dict((ws.title, ws) for ws in book.worksheets)
-#   save the result to a new worksheet
-    outpt_sheet_name = ('%s_%s_%s_%s' %(hemisphere, parcel_altas, model, y))
-    result_dataframe.to_excel(writer, outpt_sheet_name)
+    writer = pd.ExcelWriter(output_predict_score_excel_file)
+    output_sheet_name = ('%s_%s_%s_%s' %(hemisphere, parcel_altas, model, y))
+    result_dataframe.to_excel(writer, output_sheet_name)
     writer.save()
 
+    result_dataframe.plot()
+    plt.title('%s_%s_%s_%s' %(hemisphere, parcel_altas, model, y))
+    plt.savefig('/hpc/crise/hao.c/model_result/r2score_{}_{}_{}_{}.png'.format(hemisphere, parcel_altas, model, y))
 
-#   mean score of this set of LOSO
-    d = {'altas': parcel_altas, 'hemisphere': hemisphere, 'model':model, 'y_file' :y, 'mean_MAE': result_dataframe.mean(0)[0], 'R2':result_dataframe.mean(0)[1]}
-
-    dlist.append(d)
 print dlist
+
+
 
