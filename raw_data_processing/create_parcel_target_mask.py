3a079e014a9621385b3c04f69cd9407811ec5716
hao.c <c.hao.11@groupe-esigelec.fr>
2016-06-28 15:38:44 +0200

    check teh number of targets in all masks

--- raw_data_processing/create_parcel_target_mask.py
+++ raw_data_processing/create_parcel_target_mask.py
@@ -1,8 +1,15 @@
 #! /usr/bin/python -u
 # coding=utf-8
-
+"""
 # hao.c
-# instead of use fslmath to create the mask, we recommend to use this python scprit
+# 28/06/2016
+
+instead of use fslmath to create the mask, we recommend to use this python scprit
+this scripts creates the volumetric seed and taregt mask use desikan, destreiux and wmparc altas
+wmparc2009 and wmparc.
+Note: wmparc2009 parcellation file is create by mri_aparc2aseg
+see more in : /hpc/crise/hao.c/python_scripts/raw_data_processing/convert_Freesurf_parcel2nii.py
+"""
 
 import nibabel
 import os
@@ -32,12 +39,12 @@ def mask_seed(hemisphere, altas_name, label, seed_name):
     return output_seek_mask_path
 
 
-# create target mask based on the gray matter cortical parcellation: aparc(aparcaseg) or aparc2009(destrieux)
-def mask_GM(seed_path, target_altas, target_name):
-    print "create gray matter target: "+target_name
-    mask_path = op.join(mask_dir, target_name)
-    altas_path = op.join(fs_parcel_dir, '%s.nii' %target_altas)
+# create target mask based on the gray matter cortical parcellation: desikan or destrieux
+def mask_gm(seed_path, target_altas, output):
+    print "create gray matter target: "+output
+    out_put_mask_path = op.join(mask_dir, output)
 
+    altas_path = op.join(fs_parcel_dir, '%s.nii' %target_altas)
     altas_nii = nibabel.load(altas_path)
     mask = altas_nii.get_data()
     seed_mask = nibabel.load(seed_path).get_data()
@@ -55,48 +62,64 @@ def mask_GM(seed_path, target_altas, target_name):
     # print np.unique(mask)
 
     img = nibabel.Nifti1Image(mask, altas_nii.get_affine())
-    img.to_filename(mask_path)
-    print("save target mask: " + mask_path)
+    img.to_filename(out_put_mask_path)
+    print("save target mask: " + out_put_mask_path)
+
 
 # create target mask with white matter parcellation.
 # The white matter parcellation is derived from the cortical parcellation.
 # We calculate the intersection of seed region defined from wmparc2009 and the wmparc
-def mask_WM(subject, hemisphere ):
-    print "create target mask by using wmparc for "+ subject+ hemisphere
-    wmparc_path = '/hpc/crise/hao.c/data/%s/parc_freesurfer/wmparc.nii' %subject
+def mask_wm( seed_label , wmparc1, wmparc2):
+    print "create target mask by using %s  "%altas
+
+    wm1_name = '%s_target_mask_%s_%s.nii.gz' %(hemi, wmparc1, seed_name)
+    wm2_name = '%s_target_mask_%s_%s.nii.gz' %(hemi, wmparc2, seed_name)
+    out_put_wmmask1_path = op.join(mask_dir, wm1_name)
+    out_put_wmmask2_path = op.join(mask_dir, wm2_name)
+
+    wmparc_path = op.join(fs_parcel_dir, '%s.nii' %wmparc1)
     wmparc_nii = nibabel.load(wmparc_path)
-    wmparc = wmparc_nii.get_data()
-    print 'total number of altas label: '+ str(len(np.unique(wmparc)))
-    # mask_aparc(altas, seed_mask)
+    wmparc1 = wmparc_nii.get_data()
 
-    wmparc2009 = nibabel.load('/hpc/crise/hao.c/data/%s/parc_freesurfer/wmparc2009.nii'%subject).get_data()
-    if hemisphere=='lh':
-        indice_wm = np.where((wmparc2009 ==13134) | (wmparc2009 ==13174) | (wmparc2009 ==11134) | (wmparc2009 ==11174)  )
-    elif hemisphere=='rh':
-        indice_wm = np.where((wmparc2009 ==14134) | (wmparc2009 ==14174) | (wmparc2009 ==12134) | (wmparc2009 ==12174)  )
-    else:
-        print'hemisphere unknown'
+    wmparc2_path = op.join(fs_parcel_dir, '%s.nii' %wmparc2)
+    wmparc2 = nibabel.load(wmparc2_path).get_data()
 
-    wmparc[indice_wm]=0
+    wmlabel = (np.asarray(seed_label)+2000).tolist()
+    label = seed_label + wmlabel
+    print(label)
+    for i in label:
+        indice_wm = np.where(wmparc1 == i)
+        wmparc1[indice_wm] =0
+        wmparc2[indice_wm] = 0
 
     # remove unusable labels:
     remove_label = [0,2,4,5,7,8,14,15,16,24,28,30,31,41,43,44,46,47,60,62,63,72,77,80,85,1000, 2000, 3000,4000,5001,5002]
     for i in remove_label:
-        wmparc[wmparc==i]=0
+        wmparc1[wmparc1 == i] = 0
+        wmparc2[wmparc2 == i] = 0
 
     # save target mask
-    print 'wmparc target ' + str(len(np.unique(wmparc)))
+    print 'wmparc targets ' + str(len(np.unique(wmparc1)))
     #print np.unique(wmparc)
-    img = nibabel.Nifti1Image(wmparc, wmparc_nii.get_affine())
-    img.to_filename( '/hpc/crise/hao.c/data/%s/freesurfer_seg/%s_target_mask_wmparc.nii.gz' %(subject, hemisphere))
+    img = nibabel.Nifti1Image(wmparc1, wmparc_nii.get_affine())
+    img.to_filename( out_put_wmmask1_path)
+    print'save target mask: %s' %out_put_wmmask1_path
+
+    print 'wmparc targets ' + str(len(np.unique(wmparc2)))
+    #print np.unique(wmparc)
+    img = nibabel.Nifti1Image(wmparc2, wmparc_nii.get_affine())
+    img.to_filename( out_put_wmmask2_path)
+    print'save target mask: %s' %out_put_wmmask2_path
 
 
-# ======================main============================================================================================
 
-#hemi = 'rh'
-hemi = str(sys.argv[1])
-altas = str(sys.argv[2]) # aparc ou aparc2009/destrieux
-#altas = 'aparc+aseg'
+
+# ====================== main code ============================================================================================
+
+hemi = 'rh'
+#hemi = str(sys.argv[1])
+#altas = str(sys.argv[2]) # aparc/desikan ou aparc2009/destrieux
+altas = 'desikan'
 
 seed_name = 'big_STS+STG'
 
@@ -109,12 +132,13 @@ subject_list = os.listdir(root_dir)
 if seed_name=='small_STS+STG':
     seed_altas = 'destrieux'
     seed_label = [11134,11174]
+
 elif seed_name=='big_STS+STG':
-    seed_altas  ='aparc+aseg'
+    seed_altas  ='desikan'
     seed_label = [1001, 1015, 1030, 1034]
 
 if hemi=='rh':
-    seed_label = np.asarray(seed_label)+1000
+    seed_label = (np.asarray(seed_label) + 1000).tolist()
 print seed_label
 
 #======================== make mask============================================================
@@ -122,15 +146,43 @@ for i in subject_list:
     mask_dir = op.join(root_dir, i, 'freesurfer_seg')
     fs_parcel_dir = op.join(root_dir, i,'parc_freesurfer')
     seed_path = op.join(mask_dir, '%s_%s.nii.gz'%(hemi, seed_name))
-
 #   create seed mask:
     if not op.isfile(seed_path):
         mask_seed(hemi, seed_altas,  seed_label, seed_name)
 
-#   create target mask:
-    mask_GM(seed_path, altas, target_name)
+#   create gray matter parcellation target mask:
+    mask_gm(seed_path, altas, target_name)
 
-#   mask_aparc(i,hemi, 'aparc')
+#   create white matter parcellation target mask: wmparc and wmparc2009
+    if seed_altas =='desikan':
+        wmparc1= 'wmparc'
+        wmparc2 = 'wmparc2009'
+
+    elif seed_altas=='destrieux':
+        wmparc1 = 'wmparc2009'
+        wmparc2 = 'wmparc'
+
+    mask_wm(seed_label, wmparc1, wmparc2)
+
+
 print 'done! '
 
 
+# this is only used to check if the number of targets in the mask are the same  for all the subject
+"""
+add = []
+for i in subject_list:
+    mask = '/hpc/crise/hao.c/data/%s/freesurfer_seg/lh_target_mask_wmparc2009_big_STS+STG.nii.gz'%i
+    mask_nii = nibabel.load(mask).get_data()
+    affine = nibabel.load(mask).get_affine()
+    n_target = len(np.unique(mask_nii))
+    if n_target!= 310:
+        add.append(i)
+
+        for j in [11134,13134]:
+            mask_nii[mask_nii==j]=0
+        img = nibabel.Nifti1Image(mask_nii, affine)
+        img.to_filename( mask)
+        print'save target mask: %s' %mask
+
+"""
