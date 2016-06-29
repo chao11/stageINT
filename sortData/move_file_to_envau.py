7a8ec0cf8c4645e77c428241c1374414370ceba9
hao.c <c.hao.11@groupe-esigelec.fr>
2016-06-29 16:24:14 +0200

    change the tracto name

--- sortData/move_file_to_envau.py
+++ sortData/move_file_to_envau.py
@@ -7,7 +7,7 @@ import sys
 import os.path as op
 
 
-tracto_name = ''
+tracto_name = 'tracto_surface/LH_big_STS+STG_destrieux_500'
 
 #tracto_name = str(sys.argv[1])
 
@@ -18,7 +18,7 @@ for subject in subjects_list:
     subject_dir = op.join(root_dir,subject)
     #tracto_dir = op.join(subject_dir,'tracto','{}_STS+STG_{}'.format(hemisphere.upper(),tarcto_parcel))
 
-    tracto_dir = op.join(subject_dir,'tracto','%s'%(tracto_name))
+    tracto_dir = op.join(subject_dir,'%s'%(tracto_name))
   #  cmd = 'mv /hpc/crise/hao.c/data/%s/raw_dwi /envau/work/crise/hao.c/data/%s/' %(subject,subject)
 
     if op.isfile('%s/fdt_matrix2.zip'%tracto_dir):
