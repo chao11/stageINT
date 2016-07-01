a9bcac9925a1d45ae3e71bd98135ae27d1f7226f
hao.c <c.hao.11@groupe-esigelec.fr>
2016-07-01 17:53:29 +0200

    change name for different tracto

--- sortData/zip_matrix_dotfile.py
+++ sortData/zip_matrix_dotfile.py
@@ -7,9 +7,9 @@ import sys
 import os.path as op
 
 
-tracto_name = 'tracto_volume/LH_big_STS+STG_destrieux_500'
+#tracto_name = 'tracto_surface/LH_small_STS+STG_destrieux_5000'
 
-#tracto_name = str(sys.argv[1])
+tracto_name = str(sys.argv[1])
 
 root_dir = '/hpc/crise/hao.c/data'
 subjects_list = os.listdir(root_dir)
