e598b7ec0de062f55d7435f605f931110d9a4a3a
hao.c <c.hao.11@groupe-esigelec.fr>
2016-07-11 17:24:38 +0200

    add plot correlation

--- modeling/seedROI_functional_map.py
+++ modeling/seedROI_functional_map.py
@@ -27,22 +27,23 @@ def read_coord(file_path):
 
 fs_exec_dir = '/hpc/soft/freesurfer/freesurfer/bin'
 
-subject = 'AMA02'
-contrast = 'rspmT_0001'
-hemisphere = 'lh'
+subject = 'AHS22'
+contrast = 'rcon_0004'
+hemisphere = 'rh'
 altas = 'destrieux'
+tracto_name = 'tracto_volume/'
 
-coord_path = '/hpc/crise/hao.c/data/{}/tracto/{}_STS+STG_{}/coords_for_fdt_matrix2'.format(subject, hemisphere.upper(),altas)
+coord_path = '/hpc/crise/hao.c/data/{}/tracto_volume/{}_STS+STG_{}_2/coords_for_fdt_matrix2'.format(subject, hemisphere.upper(),altas)
 y_path = '/hpc/banco/voiceloc_full_database/func_voiceloc/{}/nomask_singletrialbetas_spm12_stats/resampled_fs5.3_space/{}.nii'.format(subject, contrast)
-y_mask_nii_path = '/hpc/crise/hao.c/data/{}/{}_{}_{}_mask.nii.gz'.format(subject, hemisphere, altas, contrast)
-y_mask_gii_path = '/hpc/crise/hao.c/data/{}/{}_{}_{}_mask.gii'.format(subject, hemisphere, altas, contrast)
+y_mask_nii_path = '/hpc/crise/hao.c/data/{}/{}_{}_mask.nii.gz'.format(subject, hemisphere, contrast)
+y_mask_gii_path = '/hpc/crise/hao.c/data/{}/{}_{}_mask.gii'.format(subject, hemisphere, contrast)
 
 
 coord = read_coord(coord_path)
 y_data = nibabel.load(y_path).get_data()
 img = np.zeros((256,256,256))
 for i in coord:
-    img[i[0],i[1],i[2]] = y_data[i[0],i[1],i[2]]
+    img[i[0], i[1], i[2]] = y_data[i[0], i[1], i[2]]
 
 y_img = nibabel.Nifti1Image(img, nibabel.load(y_path).get_affine())
 y_img.to_filename(y_mask_nii_path)
