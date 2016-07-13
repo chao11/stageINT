282a5039016ef8fc4e78b550a8f3ae06fbbe5076
hao.c <c.hao.11@groupe-esigelec.fr>
2016-07-13 17:33:44 +0200

    project predict pams on white surface

--- modeling/project_predictmap.py
+++ modeling/project_predictmap.py
@@ -23,26 +23,26 @@ subjects_list = os.listdir(root_dir)
 
 for subject in subjects_list[0:2]:
     for parcel_altas in altas_list[0:1]:
-        for hemisphere in hemi:
-            for y_file in y_list:
+        for hemisphere in hemi[1:]:
+            for y_file in y_list[0:1]:
                 for norma in options:
 
                     cmd = '%s/mris_convert /hpc/banco/voiceloc_full_database/fs_5.3_sanlm_db/%s/surf/%s.inflated  ' \
                           '/hpc/crise/hao.c/data/%s/surface/%s.inflated.gii' % (fs_exec_dir, subject, hemisphere, subject, hemisphere)
                     # commands.getoutput(cmd)
 
-                    tracto_dir = 'tracto_volume/{0}_STS+STG_{1}_2'.format(hemisphere.upper(), parcel_altas)
+                    tracto_dir = 'tracto_volume/%s_STS+STG_%s_2' % (hemisphere.upper(), parcel_altas)
                     predict_subject = op.join(root_dir, subject, tracto_dir, 'predict')
-                    predict_file_name = '{0}Weighted_{1}_{2}_{3}_{4}_{5}_predi_{6}'\
-                        .format(weight, hemisphere, lateral, model, norma, parcel_altas, y_file)
+                    predict_file_name = '%sWeighted_%s_%s_%s_%s_%s_predi_%s'\
+                        % (weight, hemisphere, lateral, model, norma, parcel_altas, y_file)
 
                     predict_nii_path = op.join(predict_subject, '%s.nii.gz' % predict_file_name)
                     proj2surf_path = op.join(predict_subject, '%s.gii' % predict_file_name)
 
                     if op.isfile(predict_nii_path) and (not op.isfile(proj2surf_path)):
 
-                        cmd = '{}/mri_vol2surf --src {} --regheader {} --hemi {} --o {}  --out_type gii --projfrac 0.5' \
-                              ' --surf white'.format(fs_exec_dir, predict_nii_path, subject, hemisphere, proj2surf_path)
+                        cmd = '%s/mri_vol2surf --src %s --regheader %s --hemi %s --o %s  --out_type gii --projfrac 0.5' \
+                              ' --surf white' % (fs_exec_dir, predict_nii_path, subject, hemisphere, proj2surf_path)
                         print cmd
                        # commands.getoutput(cmd)
                     elif not op.isfile(predict_nii_path):
